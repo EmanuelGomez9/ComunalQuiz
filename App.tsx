8898099086a5ab44ec817230ef4bbee56afb42d6
Emanuel Gomez <Emanuel.Gomez.9@gmail.com>
2022-03-01 15:57:43 -0500

    some view fixes,fixes on functions, just need to return the result on the view and try recursive if the first number possible wasnt the one but may next one may be

--- App.tsx
+++ App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
@@ -23,48 +13,9 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{
-  stations: string,
-  costs: string;
-}> = ({children, stations, costs }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   const stringToNumberArray = (stringArray: string) => {
     return stringArray.split(",").map(elem => {
       let n:number = Number(elem);
@@ -74,8 +25,11 @@ const App = () => {
 
   const pickStartIndex = (stations: [], costs: [], previousIndex: number = 0 ) => {
     let startPointIndex : number = -1;
-    for (let i:number = previousIndex; i < stationArr.length; i++) {
-      startPointIndex = stations[i] > costs[i] ? startPointIndex : i;
+    for (let i:number = previousIndex; i < stations.length; i++) {
+      if (stations[i] > costs[i]) {
+        startPointIndex = i;
+        break;
+      }
     }
     return startPointIndex;
  }
@@ -84,78 +38,89 @@ const App = () => {
     let currentGas: number = stations[startPoint];
     for (let i:number = startPoint; i < stations.length; i++) {
       if (i !== stations.length-1) {
-        currentGas = stations[i] - costs[i] + stations[i+1];
+        currentGas = currentGas - costs[i] + stations[i+1];
       } else {
-        currentGas = stations[i] - costs[i] + stations[0];
+        currentGas = currentGas - costs[i] + stations[0];
       };
-      if (currentGas <= 0) break;
-    }
-    if (currentGas>costs[costs.length]) {
-      for (let j:number = 0; j = startPoint; j++) {
-
-      }
-    }
+      if (currentGas <= 0) return -1;
+    };
+    if (startPoint!==0) {
+        if (currentGas>costs[costs.length-1]) {
+          for (let j:number = 0; j < startPoint; j++) {
+            if (j!==startPoint-1) {
+                currentGas = currentGas - costs[j] + stations[j+1];
+            } else {
+                if (currentGas - costs[startPoint-1] >= 0) {
+                    return startPoint;
+                };
+            };
+          };
+        } else {
+            return -1
+        };
+    };
   }
 
   const doTheTrick = (stations: string, costs: string) => {
-    console.log(stations);
-    console.log(costs);
-    //let stationsArray: [] = stringToNumberArray(stations);
-    //let costsArray: [] = stringToNumberArray(costs);
-    //let startPoint: number = pickStartIndex(stationsArray, costsArray);
-    //doRoute(stations, costsArray, startPoint);
+    let result: number = -1;
+    let stationsArray: [] = stringToNumberArray(stations);
+    let costsArray: [] = stringToNumberArray(costs);
+    let startPoint: number = pickStartIndex(stationsArray, costsArray);
+    if (startPoint > -1) {
+        let result = doRoute(stationsArray, costsArray, startPoint);
+
+    }
   }
 
   const [stations, setStations] = useState('');
   const [costs, setCosts] = useState('');
+  const [result, setResult] = useState('');
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+        <View style={styles.sectionContainer}>
             <TextInput
                 value={stations} onChangeText={ newStations => setStations(newStations)}
                 placeholder="stations" keyboardType="numeric"
-                style={{height: 40, borderColor: 'white', borderWidth: 1}}
+                style={styles.inputs}
                 />
             <TextInput
                 value={costs} onChangeText={ newCosts => setCosts(newCosts)}
                 placeholder="costs" keyboardType="numeric"
-                style={{height: 40, borderColor: 'green', borderWidth: 1}}
+                style={styles.inputs}
                 />
             <Button
                 title="Calculate"
                 onPress={ () => doTheTrick(stations, costs)}
               />
+            <Text style={styles.result}> The Result is: {result} </Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
+    flex: 1,
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputs: {
+    display: "flex",
+    height: 40,
+    marginBottom: 10,
+    width: 300,
+    backgroundColor: "#000000",
+    borderColor: "gray",
+    borderWidth: 1
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  result: {
+    backgroundColor: "green",
+    width: 300,
+    height: 40,
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    marginTop: 10,
+  }
 });
 
 export default App;
